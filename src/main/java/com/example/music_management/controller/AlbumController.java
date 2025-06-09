6a274e52e7d7936b268f8e4c91b4108ccfd9da95
[java-yuya] <[ogasawarayu@cy-com.co.jp]>
2025-06-09 15:42:30 +0900

    test1 2

--- src/main/java/com/example/music_management/controller/AlbumController.java
+++ src/main/java/com/example/music_management/controller/AlbumController.java
@@ -120,5 +120,10 @@ public class AlbumController {
     public String test1() {
         return "test1";
     }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "test2";
+    }
     
 }
